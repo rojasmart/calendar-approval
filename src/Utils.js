090117258d0c78e84e5cf2468a617ed2b449e3e2
binarypsilocybin <rogeriosvaldo@gmail.com>
2025-02-22 16:20:12 +0000

    --add new styles

--- src/Utils.js
+++ src/Utils.js
@@ -1,5 +1,8 @@
 // Função para normalizar o nome
 export function normalizeName(name) {
+  if (!name) {
+    return "";
+  }
   return name
     .split(" ")
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
