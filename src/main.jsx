b22249878d9e189b103809c85e8ff37cd3757039
binarypsilocybin <rogeriosvaldo@gmail.com>
2025-03-23 18:36:15 +0000

    --add implement color mode toggle and theme configuration for improved UI responsiveness

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "../src/index.css";
 import App from "./App.jsx";
+import theme from "./theme.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ChakraProvider>
+    {/* The ColorModeScript needs to come before ChakraProvider */}
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </StrictMode>
